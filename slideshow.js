9c25a3d35fe366e9aa4330266f52e44a5dbe6d8a
Ziheng Liao <zlia0050@student.monash.edu>
2023-02-17 16:41:54 +1100

    completed slideshow and responsiveness
    can now switch around the images and the images are not distorted when the window is resized

--- slideshow.js
+++ slideshow.js
@@ -6,11 +6,20 @@ buttons.forEach(button => {
     button.addEventListener("click", () => {
         // console.log(button.dataset)
         const offset = button.dataset.slideshowButton === "next" ? 1 : -1   // the name slideshowbutton has to match up to the name after the data attribute
-        console.log(offset)
+        const slides = button.closest("[data-slideshow]").querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        console.log(slides.children)
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+        console.log(newIndex)
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
     }
 
     )
-    // console.log("hi")
+
     
 });
 
