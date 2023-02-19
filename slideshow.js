51cd60b5afac7ae941f1ba134b605f97b92858e1
Ziheng Liao <zlia0050@student.monash.edu>
2023-02-20 00:43:13 +1100

    readjustments to the page and added photo gallery
    gave spacing in the review area. used flexbox
    
    added media queries
    
    got rid of buttons as well

--- slideshow.js
+++ slideshow.js
@@ -1,24 +1,31 @@
-const buttons = document.querySelectorAll("[data-slideshow-button]")
+// const buttons = document.querySelectorAll("[data-slideshow-button]")
+
+const buttons = document.querySelectorAll("[data-slideshow]")
+
+
+
 
 
 
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        // console.log(button.dataset)
-        const offset = button.dataset.slideshowButton === "next" ? 1 : -1   // the name slideshowbutton has to match up to the name after the data attribute
-        const slides = button.closest("[data-slideshow]").querySelector("[data-slides]")
 
+    interval = setInterval(() => {
+        const offset = 1   // the name slideshowbutton has to match up to the name after the data attribute
+        const slides = button.querySelector("[data-slides]")
         const activeSlide = slides.querySelector("[data-active]")
-        console.log(slides.children)
+    
+        
+        // console.log(slides.children)
         let newIndex = [...slides.children].indexOf(activeSlide) + offset
         if (newIndex < 0) newIndex = slides.children.length - 1
         if (newIndex >= slides.children.length) newIndex = 0
-        console.log(newIndex)
+        // console.log(newIndex)
         slides.children[newIndex].dataset.active = true
         delete activeSlide.dataset.active
     }
+    , 5000)
 
-    )
+    
 
     
 });
@@ -28,11 +35,24 @@ buttons.forEach(button => {
 
 
 
+// button.addEventListener("click", () => {
+//     // console.log(button.dataset)
+//     clearInterval(interval)
+//     const offset = button.dataset.slideshowButton === "next" ? 1 : -1   // the name slideshowbutton has to match up to the name after the data attribute
+//     const slides = button.closest("[data-slideshow]").querySelector("[data-slides]")
+//     const activeSlide = slides.querySelector("[data-active]")
 
+    
+//     // console.log(slides.children)
+//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
+//     if (newIndex < 0) newIndex = slides.children.length - 1
+//     if (newIndex >= slides.children.length) newIndex = 0
+//     console.log(newIndex)
+//     slides.children[newIndex].dataset.active = true
+//     delete activeSlide.dataset.active
+// }
 
-
-
-
+// )
 
 
 
